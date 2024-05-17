1ef0eb14a9b851c999a2716f6c2b903842d116e8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-17 17:30:43 +0300

    header is now rendering and add new button's functionality tested with dummy test data

---
+++ src/app.js
@@ -0,0 +1,38 @@
+"use strict";
+class NoteItem {
+    constructor() {
+        this.butn = document.querySelector(".add-btn");
+    }
+    addNote() { }
+}
+class Note {
+    constructor(id = Math.random(), content, date, title) { }
+}
+class Notes {
+    constructor() {
+        this.templateElement = document.getElementById("single-note");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
+class NoteHeader {
+    constructor() {
+        this.templateElement = document.getElementById("tmp-note-container-header");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+        this.btn = document.getElementById("add-btn");
+        this.btn.addEventListener("click", this.addNote.bind(this));
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+    addNote() { }
+}
+const el = new NoteHeader();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,51 @@
- 
+class NoteItem {
+  private note: Note;
+  content: string;
+  id: number;
+  title?: string;
+  date: Date;
+  butn = document.querySelector(".add-btn")! as HTMLButtonElement;
+  constructor() {}
+  addNote() {}
+}
+class Note {
+  constructor(id: number = Math.random(), content: string, date: Date, title?: string) {}
+}
+
+class Notes {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLDivElement;
+  constructor() {
+    this.templateElement = document.getElementById("single-note") as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLDivElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+class NoteHeader {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLDivElement;
+  btn: HTMLButtonElement;
+  constructor() {
+    this.templateElement = document.getElementById("tmp-note-container-header") as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLDivElement;
+    this.attach();
+
+    this.btn = document.getElementById("add-btn")! as HTMLButtonElement;
+    this.btn.addEventListener("click", this.addNote.bind(this));
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+  private addNote() {}
+}
+const el = new NoteHeader();
