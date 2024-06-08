07ba94e7c886dc9f079f13dd395588804e5d3b45
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-08 06:47:56 +0300

    remove: unused js file after wp optimization

--- src/Objects/Note.js
+++
@@ -1,9 +0,0 @@
-export default class Note {
-    constructor(id, content, date, title, checks) {
-        this.id = id;
-        this.content = content;
-        this.date = date;
-        this.title = title;
-        this.checks = checks;
-    }
-}
--- src/app.js
+++
@@ -1,4 +0,0 @@
-import { noteOperations } from "./Components/NoteOperations.js";
-import { header } from "./Objects/Header.js";
-const headerComponent = header;
-const noteOperationsInstance = noteOperations;
